36be5dde7148a33024f9d8667dc6c42075f7ad6f
Javi <javi.cb@gmail.com>
2021-02-01 10:31:53 +0100

    editar heroe en backend

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -30,19 +32,34 @@ export class AgregarComponent implements OnInit {
     alt_img: ''
   };
 
-  constructor(private heroeService: HeroesService) { }
+  constructor(private heroeService: HeroesService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroeService.getHeroeById(id))
+      )
+      .subscribe(response => this.heroe = response);
   }
 
   guardar(): void {
     if (this.heroe.superhero.trim().length === 0) {
       return;
     }
-    this.heroeService.agregarHeroe(this.heroe)
-      .subscribe(response => {
-        console.log('Respuesta: ', response);
-      });
+
+    if (this.heroe.id) {
+      // Actualizar
+      this.heroeService.actualizarHeroe(this.heroe)
+        .subscribe(response => {
+          console.log('Héroe actualizado: ', response);
+        });
+    } else {
+      // Crear
+      this.heroeService.agregarHeroe(this.heroe)
+        .subscribe(response => {
+          this.router.navigate(['/heroes/editar', response.id]);
+        });
+    }
   }
 
 }
