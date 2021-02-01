80d370eea4b0a559bdf0c26fc52237d93dab9539
Javi <javi.cb@gmail.com>
2021-02-01 12:31:13 +0100

    auth service

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,19 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   login(): void {
-    this.router.navigate(['/heroes']);
+    this.authService.login()
+      .subscribe(resp => {
+        console.log(resp);
+        if (resp.id) {
+          this.router.navigate(['/heroes']);
+        }
+      });
   }
 
 }
