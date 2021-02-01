87fbe05da3053c7b2d256d1194c493b8055d1933
Javi <javi.cb@gmail.com>
2021-02-01 12:44:38 +0100

    mostrar información de usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth';
 
@@ -10,10 +11,18 @@ import { Auth } from '../interfaces/auth';
 export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
+  private AUTH: Auth | undefined;
+
+  get auth(): Auth {
+    return { ...this.AUTH! };
+  }
 
   constructor(private http: HttpClient) { }
 
   login(): Observable<Auth> {
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+      .pipe(
+        tap(resp => this.AUTH = resp)
+      );
   }
 }
