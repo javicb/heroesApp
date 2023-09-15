544df4b273de4b28627cb3db3a12a752ec815ecc
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-09-16 00:42:11 +0200

    change app name

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# MarvelHeroesApp
+# HeroesApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.1.
-
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'marvelHeroesApp'`, () => {
+  it(`should have as title 'heroesApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('marvelHeroesApp');
+    expect(app.title).toEqual('heroesApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('marvelHeroesApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('heroesApp app is running!');
   });
 });
